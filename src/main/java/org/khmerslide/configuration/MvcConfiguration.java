621b41248ef47816fd5cfc63b6a27efce3ec3fd3
NemSothea <nemsothea13@gmail.com>
2016-08-13 15:31:30 +0700

    Add Done Comment For Select DATA

--- src/main/java/org/khmerslide/configuration/MvcConfiguration.java
+++ src/main/java/org/khmerslide/configuration/MvcConfiguration.java
@@ -25,6 +25,7 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		registry.addViewController("/user/index").setViewName("/user/index");
 		
 		registry.addViewController("/user/home").setViewName("/user/index");
+		registry.addViewController("/user/view-history").setViewName("/user/view-history");
 //		registry.addViewController("/user").setViewName("/user/user");
 //		registry.addViewController("/dba").setViewName("/dba");
 		registry.addViewController("/login").setViewName("/login");
